2bed5ac37b15c906578b0c1c5c5efa4227f29241
ahnoamu <ahnoamu@gmail.com>
2019-03-14 15:29:37 +0200

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -3,3 +3,15 @@ cat /etc/lsb-release
 #or
 cat /etc/linuxmint/info
 
+#No launcher and Menu bar on ubuntu Fix 
+#Using deconf
+sudo apt-get install dconf-tools
+dbus-launch dconf reset -f /org/compiz/
+sudo reboot
+
+#OR
+#Using unity
+sudo apt-get update
+sudo apt-get install --reinstall ubuntu-desktop
+sudo apt-get install unity
+unity --reset 
