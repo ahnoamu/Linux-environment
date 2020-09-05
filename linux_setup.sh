661fd3839b7d0f27dcf0cf3257329e614e283148
ahnoamu <ahnoamu@gmail.com>
2020-09-05 23:09:57 +0200

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,8 +1,14 @@
+#========================
 #Check linux distribution
 cat /etc/lsb-release
 #or
 cat /etc/linuxmint/info
 
+#Check latest available package to install from repo
+apt-cache policy <packageName>
+
+
+#========================
 #No launcher and Menu bar on ubuntu Fix 
 #Using deconf
 sudo apt-get install dconf-tools
@@ -15,3 +21,30 @@ sudo apt-get update
 sudo apt-get install --reinstall ubuntu-desktop
 sudo apt-get install unity
 unity --reset 
+
+#========================
+#Install biotools
+sudo apt-get install tk-dev
+sudo apt-get install bwidget
+
+#Add installation repo
+sudo add-apt-repository ppa:ubuntugis/ppa
+#update
+sudo apt-get update
+#Install gdal
+sudo apt-get install libudunits2-dev libgdal-dev libgeos-dev libproj-dev #updates gdal version
+#else 
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get -y install python-gdal             #Installs gdal
+sudo apt -y install gdal-bin python3-gdal       #python3
+#Install sf using conda (unproblematic)
+conda create -n rsf -c conda-forge r-sf
+source activate rsf
+# In R terminal
+install.packages("rpanel", dependencies = TRUE)
+install.packages("SpatialEpi", dependencies=TRUE)
+
+
+
+
