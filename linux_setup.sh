67bd7c94f0876d0c2de53a00be8f37d735e16d26
ahnoamu <ahnoamu@gmail.com>
2020-09-16 22:57:10 +0200

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -22,40 +22,4 @@ sudo apt-get install --reinstall ubuntu-desktop
 sudo apt-get install unity
 unity --reset 
 
-#========================
-#Install biotools
-sudo apt-get install tk-dev
-sudo apt-get install bwidget
-#Add installation repo
-sudo add-apt-repository ppa:ubuntugis/ppa
-#update
-sudo apt-get update
-#Install gdal  * ensure only a single version/installation is done, else problems downstream with installation of spatialEpi)
-sudo apt-get install libudunits2-dev libgdal-dev libgeos-dev libproj-dev #updates gdal version
-#else 
-sudo apt-get update
-sudo apt-get install gdal-bin
-sudo apt-get -y install python-gdal             #Installs gdal
-sudo apt -y install gdal-bin python3-gdal       #python3
-#Install "sf" and "foreign" using conda (unproblematic)
-conda create -n rsf -c conda-forge r-sf
-source activate rsf
-conda install -c conda-forge r-foreign 
-# In R terminal
-install.packages("rpanel", dependencies = TRUE)
-install.packages("SpatialEpi", dependencies=TRUE)
-## Activate conda environment before running R
-#========================
-
-
-#Upgrade packages in R after r-base version upgrade
-#Reference: https://github.com/rocker-org/shiny/issues/48
-#This is done in R shell 
-new_packages <- installed.packages()
-df <- as.data.frame(new_packages)
-D2 = subset(df, !(Built %in% c('4.0.0','4.0.2')))
-D2 <- as.data.frame(D2)
-install.packages(D2$Package)
-#===========================
-
 
